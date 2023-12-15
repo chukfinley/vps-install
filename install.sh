261623ae669a0c27be11241f290a2f4ba3bcd9d5
hackerman <77645077+chukfinley@users.noreply.github.com>
2023-12-15 21:33:33 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp btop thefuck python3-pip -y
+chsh -s $(which zsh) ${USER}
+wget https://gist.githubusercontent.com/chukfinley/afe0108991641e37548d1789ff7fbcfb/raw/3ceddec210fe919730cd0555d95b524741284951/.zshrc -O .zshrc
+cd  && curl https://raw.githubusercontent.com/chukfinley/advcpmv/master/install.sh --create-dirs -o ./advcpmv/install.sh && (cd advcpmv && sh install.sh)
+cd && curl https://pyenv.run | bash
