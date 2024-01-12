ab1b8bcb4a82a3c88efeaebefe163f2724d9c192
hackerman <77645077+chukfinley@users.noreply.github.com>
2024-01-12 15:15:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #/usr/bin/env bash
+GREEN='\e[32m'
+RESET='\e[0m'
 cd
-echo "Updating"
-sudo apt update > /dev/null 2>&1
-echo "Upgrading this may take a while" 
-sudo apt upgrade -y > /dev/null 2>&1
-echo "Installing Packages this may take a while"
-sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp btop thefuck python3-pip git cmake make gcc build-essential docker docker.io docker-compose tmux net-tools curl wget -y > /dev/null 2>&1
-echo "Setiing zsh as shell"
+echo -e "${GREEN}Updating${RESET}"
+sudo apt update
+echo -e "${GREEN}Upgrading this may take a while${RESET}" 
+sudo apt upgrade -y
+echo -e "${GREEN}Installing Packages this may take a while${RESET}"
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp btop thefuck python3-pip git cmake make gcc build-essential docker docker.io docker-compose tmux net-tools curl wget -y
+echo -e "${GREEN}Setiing zsh as shell${RESET}"
 chsh -s $(which zsh) ${USER}
-echo "getting .zshrc"
-wget https://gist.githubusercontent.com/chukfinley/afe0108991641e37548d1789ff7fbcfb/raw/3ceddec210fe919730cd0555d95b524741284951/.zshrc -O .zshrc > /dev/null 2>&1
-echo "getting temux config" 
-wget https://raw.githubusercontent.com/chukfinley/configuration/main/.tmux.conf -O .tmux.conf > /dev/null 2>&1
-echo "installing advcpmv"
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -O /usr/bin/advmv > /dev/null 2>&1
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -O /usr/bin/advcp > /dev/null 2>&1
+echo -e "${GREEN}getting .zshrc${RESET}"
+wget https://gist.githubusercontent.com/chukfinley/afe0108991641e37548d1789ff7fbcfb/raw/3ceddec210fe919730cd0555d95b524741284951/.zshrc -O .zshrc
+echo -e "${GREEN}getting temux config${RESET}" 
+wget https://raw.githubusercontent.com/chukfinley/configuration/main/.tmux.conf -O .tmux.conf
+echo -e "${GREEN}installing advcpmv${RESET}"
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -O /usr/bin/advmv
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -O /usr/bin/advcp
 sudo chmod +x /usr/bin/advmv
 sudo chmod +x /usr/bin/advcp
-echo "instaling pyenv"
-cd && curl https://pyenv.run | bash > /dev/null 2>&1
-echo "removing login message"
+echo -e "${GREEN}instaling pyenv${RESET}"
+cd && curl https://pyenv.run | bash
+echo -e "${GREEN}removing login message${RESET}"
 sudo sh -c '> /etc/motd'
 #login message remove
 chmod -x /etc/update-motd.d/*
@@ -27,15 +29,15 @@ chmod -x /etc/update-motd.d/*
 sed -i 's/^PrintMotd.*/PrintMotd no/' /etc/ssh/sshd_config
 sed -i 's/^PrintLastLog.*/PrintLastLog no/' /etc/ssh/sshd_config
 if ! grep -q "^PrintMotd no" /etc/ssh/sshd_config; then
-    echo "PrintMotd no" >> /etc/ssh/sshd_config
+    echo -e "PrintMotd no" >> /etc/ssh/sshd_config
 fi
 if ! grep -q "^PrintLastLog no" /etc/ssh/sshd_config; then
-    echo "PrintLastLog no" >> /etc/ssh/sshd_config
+    echo -e "PrintLastLog no" >> /etc/ssh/sshd_config
 fi
 
 # Neustarten des SSH-Dienstes
-echo "Restating sshd"
+echo -e "${GREEN}Restating sshd${RESET}"
 systemctl restart sshd
-echo "switching to zsh"
+echo -e "${GREEN}switching to zsh${RESET}"
 zsh
-echo "done"
+echo -e "${GREEN}done${RESET}"
