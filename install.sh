3eee685682fc69a2f37a21e8a48a83ec48e3b9e1
hackerman <77645077+chukfinley@users.noreply.github.com>
2024-01-12 14:56:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,39 @@
 #/usr/bin/env bash
 cd
-sudo apt update && sudo apt upgrade -y
-sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp btop thefuck python3-pip git cmake make gcc build-essential docker docker.io docker-compose tmux net-tools curl wget -y
+echo "Updating"
+sudo apt update && sudo apt upgrade -y > /dev/null 2>&1
+echo "Installing Packages"
+sudo apt install zsh zsh-syntax-highlighting zsh-autosuggestions yt-dlp btop thefuck python3-pip git cmake make gcc build-essential docker docker.io docker-compose tmux net-tools curl wget -y > /dev/null 2>&1
+echo "Setiing zsh as shell"
 chsh -s $(which zsh) ${USER}
-wget https://gist.githubusercontent.com/chukfinley/afe0108991641e37548d1789ff7fbcfb/raw/3ceddec210fe919730cd0555d95b524741284951/.zshrc -O .zshrc
-wget https://raw.githubusercontent.com/chukfinley/configuration/main/.tmux.conf -O .tmux.conf
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -O /usr/bin/advmv
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -O /usr/bin/advcp
+echo "getting .zshrc"
+wget https://gist.githubusercontent.com/chukfinley/afe0108991641e37548d1789ff7fbcfb/raw/3ceddec210fe919730cd0555d95b524741284951/.zshrc -O .zshrc > /dev/null 2>&1
+echo "getting temux config" 
+wget https://raw.githubusercontent.com/chukfinley/configuration/main/.tmux.conf -O .tmux.conf > /dev/null 2>&1
+echo "installing advcpmv"
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -O /usr/bin/advmv > /dev/null 2>&1
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -O /usr/bin/advcp > /dev/null 2>&1
 sudo chmod +x /usr/bin/advmv
 sudo chmod +x /usr/bin/advcp
-cd && curl https://pyenv.run | bash
+echo "instaling pyenv"
+cd && curl https://pyenv.run | bash > /dev/null 2>&1
+echo "removing login message"
+sudo sh -c '> /etc/motd'
+#login message remove
+chmod -x /etc/update-motd.d/*
+
+sed -i 's/^PrintMotd.*/PrintMotd no/' /etc/ssh/sshd_config
+sed -i 's/^PrintLastLog.*/PrintLastLog no/' /etc/ssh/sshd_config
+if ! grep -q "^PrintMotd no" /etc/ssh/sshd_config; then
+    echo "PrintMotd no" >> /etc/ssh/sshd_config
+fi
+if ! grep -q "^PrintLastLog no" /etc/ssh/sshd_config; then
+    echo "PrintLastLog no" >> /etc/ssh/sshd_config
+fi
+
+# Neustarten des SSH-Dienstes
+echo "Restating sshd"
+systemctl restart sshd
+echo "switching to zsh"
 zsh
+echo "done"
